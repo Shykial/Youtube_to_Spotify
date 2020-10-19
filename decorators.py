af0406705f87f407fd3c24889414bfd0ac277345
scroofer <szymon.jeziorski@interia.pl>
2020-10-20 00:35:38 +0200

    enclosing function name in quotations

--- decorators.py
+++ decorators.py
@@ -5,6 +5,6 @@ def timer(function):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = function(*args, **kwargs)
-        print(f'Function {function.__name__} execution took: {time.time() - start_time}')
+        print(f'Function "{function.__name__}" execution took: {time.time() - start_time}')
         return result
     return wrapper
